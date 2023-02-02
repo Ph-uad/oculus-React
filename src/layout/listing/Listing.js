3431b409635ddb95c54610467e17bebdc6fbbe80
Ph-uad <temidayo26j@gmail.com>
2023-02-02 20:53:44 +0100

    implementing dynamic listing

---
+++ src/layout/listing/Listing.js
@@ -0,0 +1,41 @@
+import useFetchAPI from "../../hooks/useFetchAPI";
+import Image1 from '../../Assets/image/image1.png'
+import Image2 from '../../Assets/image/image2.png'
+import Image3 from '../../Assets/image/image3.png'
+import Image4 from '../../Assets/image/image4.png'
+
+const Listing = (props) => {
+
+    const { type } = props;
+
+    const images = type === 'sunglasses' ?  [Image1, Image2, Image3] : [Image4];
+    const filteredList = useFetchAPI().filter(x => x.type === type);
+    const glassesList = filteredList.map(item => {
+        return (
+            <div className="list card" key={ item.id }>
+                <figure className="figure list__figure">
+                    <img src={ item.image ? item.image : images[Math.floor(Math.random() * images.length)] } alt="glasses" className="img" />
+                </figure>
+
+                <div className="flex">
+                    <div className="">
+                        <h2 className="heading--tertiary">{ item.name }</h2>
+                        <h6 className="heading--senary">{ item.type }</h6>
+                    </div>
+                    <h4 className="heading--quaternary">$450</h4>
+                </div>
+            </div>
+        )
+    });
+
+
+    return (
+        <section className="section">
+            <div className="container">
+                <ul className='grid'>{ glassesList }</ul>
+            </div>
+        </section>
+    )
+}
+
+export default Listing
--- src/pages/Eyeglasses.js
+++ src/pages/Eyeglasses.js
@@ -1,36 +1,8 @@
-import React from 'react'
-import Image from '../Assets/image/glasses.jpg'
-import useFetchAPI from '../hooks/useFetchAPI'
+import Listing from "../layout/listing/Listing"
 
 const Eyeglasses = () => {
-    const filterEyeGlasses = useFetchAPI().filter(x => x.type === 'eyeglasses');
-
-    const glassesList = filterEyeGlasses.map(item => {
-        return (
-            <li className="list card" key={ item.id }>
-                <figure className="figure list__figure">
-                    <img src={ item.image ? item.image : Image } alt="glasses" className="img" />
-                </figure>
-
-                <div className="">
-                    <h2 className="heading--tertiary">{ item.name }</h2>
-                    <h4 className="heading--quaternary">{ item.type }</h4>
-                    <p className="paragraph">{ item.description }</p>
-                </div>
-            </li>
-        )
-    });
-
     return (
-        <section className="section">
-            <div className="container">
-                <ul className='grid'>
-                    { glassesList }
-                </ul>
-            </div>
-        </section>
-
+                <Listing type="eyeglasses"/>
     )
 }
-
 export default Eyeglasses
