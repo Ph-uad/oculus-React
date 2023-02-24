e74a3e84dbe0e0292327a0b6ff65fa1b93a99831
Ph-uad <temidayo26j@gmail.com>
2023-02-24 04:19:38 +0100

    route re-definition

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Footer from '../layout/footer/Footer'
+import Sidebar from  '../layout/sidebar/Sidebar'
 import Nav from '../layout/navigation/Nav'
 
 const Root = () => {
   return (
     <div>
+        <Sidebar/>
         <Nav/>
         <Outlet/>
         <Footer/>
