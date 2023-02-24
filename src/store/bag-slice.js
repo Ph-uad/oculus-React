86d2da962bdb01d1dc8e357ee204f486d9c99460
Ph-uad <temidayo26j@gmail.com>
2023-02-24 04:20:27 +0100

    adding total Amount to Cart

--- src/store/bag-slice.js
+++ src/store/bag-slice.js
@@ -1,28 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {bag: []};
+const initialState = {
+  bagItems: [],
+  bagIsVisible : false,
+  totalAmount : 0
+};
 
 const bagSlice = createSlice({
     name : 'bag',
     initialState,
     reducers  : {
         addToBag: (state, action)=>{
-          state.bag.filter((value) =>{
-            if (value.id === action.payload.id ){
-              console.log(value)
-              value.amount += 1 
-            }else {
-              console.log('differnt')
-              state.bag.push(action.payload) ;
-            }
-            return null
-          })
+          console.log(action.payload)
+          const newItem = action.payload
+          state.bagItems.push(
+            {
+              id: newItem.id,
+              image: newItem.image,
+              name: newItem.name,
+              price: newItem.price
+            })
+           state.totalAmount += newItem.price 
         },
         removeFromBag : (state,action)=>{
-          state.bag.push(action.payload) ;
+          state.bagItems.push(action.payload) ;
+        },
+        showBag : (state, action)=>{
+          console.log(state.bagIsVisible)
+          state.bagIsVisible = action.payload
         }
     }
 })
+
+bagSlice.getInitialState()
 
 export const bagActions = bagSlice.actions;
 export default bagSlice
