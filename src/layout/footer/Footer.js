16f162836361dd35191eb279ecc74ce882655c46
Ph-uad <temidayo26j@gmail.com>
2023-03-01 23:38:39 +0100

    UI flex fix

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -3,20 +3,18 @@ import classes from './footer.module.css'
 
 const Footer = () => {
     return (
-        <footer className={`${classes.footer} section`}>
-            <div className="container">
-                <div className={`${classes.flex} flex`}>
-                    <div className={classes.detail}>
-                        <h4 className="heading--quaternary">oculus</h4>
-                        <p className="paragraph">
-                            Oculus is conceptual e-commerce project based on learning design ,development
-                            with React.
-                        </p>
-                    </div>
-                    <div className="contact">
-                        <h4 className="heading--quaternary">Contact</h4>
-                        <a href="#no" className='link'><span className="paragraph">temidayo26@gmail.com</span> </a>
-                    </div>
+        <footer className={ `${classes.footer} section` }>
+            <div className={ `${classes.flex} container flex` }>
+                <div className={ classes.detail }>
+                    <h4 className="heading--quaternary">oculus</h4>
+                    <p className="paragraph">
+                        Oculus is conceptual e-commerce project based on learning design ,development
+                        with React.
+                    </p>
+                </div>
+                <div className="contact">
+                    <h4 className="heading--quaternary">Fancy Note</h4>
+                    <span className="paragraph">we know what tomorrow is <br /> cause we saw yesterday</span>
                 </div>
             </div>
         </footer>
