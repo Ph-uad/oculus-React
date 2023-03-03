a1e9fd4e1a6650728d70f3a0ab67b32060392855
Ph-uad <temidayo26j@gmail.com>
2023-03-03 05:03:16 +0100

    cart page setup

---
+++ src/layout/cart/Cart.js
@@ -0,0 +1,87 @@
+import { useDispatch, useSelector } from "react-redux"
+import { bagActions } from "../../store/bag-slice";
+import classes from './cart.module.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+
+// import required modules
+import {Pagination } from "swiper";
+
+
+
+const Cart = () => {
+
+    const items = useSelector(item => item.bag.bagItems);
+    const total = useSelector(item => item.bag.totalAmount);
+    const dispatch = useDispatch()
+
+    const addItemHandler = (item) => { dispatch(bagActions.increaseItemAmount(item)) }
+    const substractItemHandler = (item) => { dispatch(bagActions.reduceItemAmount(item)) }
+
+    const mapBagItems = items.map(item => {
+        if (!item) {
+            return <p>Nothing here</p>
+        }
+        return (
+            <SwiperSlide key={ item.id } className={ `${classes.tile}` }>
+                <figure className={ `figure ${classes.figure}` }><img src={ item.image } className="img" alt="thumbnail" /></figure>
+                <div className={ classes['list--detail'] }>
+                    <div className="flex">
+                        <div className="">
+                            <h3 className={ `heading--senary` }>{ item.name }</h3>
+                            <button className="btn--utility" onClick={ () => substractItemHandler(item) }>-</button>
+                        </div>
+                        <div className="">
+                            <h4 className="heading--senary">${ item.price }(x{ item.amount })</h4>
+                            <div className="flex text--right">
+                                <button className={ `btn--utility ${classes.right}` } onClick={ () => addItemHandler(item) } >+</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide >
+        )
+    })
+
+    return (
+        <section className="section container">
+            <h2 className="heading--tertiary">Bag</h2>
+            <Swiper
+                slidesPerView={ 1 }
+                spaceBetween={ 10 }
+                pagination={ {
+                    clickable: true,
+                } }
+                breakpoints={ {
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 4,
+                        spaceBetween: 40,
+                    },
+                    1024: {
+                        slidesPerView: 5,
+                        spaceBetween: 50,
+                    },
+                } }
+                modules={ [Pagination] }
+                className={ classes["mySwiper"] }
+            >
+                { mapBagItems }
+            </Swiper>
+
+            <h3 className={`text--right heading--secondary`}>Bill : ${total}</h3>
+        </section>
+    )
+}
+
+export default Cart
+
---
+++ src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import Cart from "../layout/cart/Cart"
+
+const CartPage = () => {
+    return (
+        <>
+          <Cart/>
+        </>
+    )
+}
+
+export default CartPage
