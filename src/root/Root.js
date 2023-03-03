9330c3124a641b2fccf4cc45adcd3604fa4455df
Ph-uad <temidayo26j@gmail.com>
2023-03-03 05:00:34 +0100

    extra dispatch method

--- src/root/Root.js
+++ src/root/Root.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../layout/footer/Footer'
 import Nav from '../layout/navigation/Nav'
-import Notification from '../layout/notification/Notification'
+import Notification from '../components/notification/Notification'
 
 const Root = () => {
   return (
--- src/store/bag-slice.js
+++ src/store/bag-slice.js
@@ -2,41 +2,56 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   bagItems: [],
-  bagIsVisible : false,
-  totalAmount : 0
+  totalAmount: 0
 };
 
 const bagSlice = createSlice({
-    name : 'bag',
-    initialState,
-    reducers  : {
-        addToBag: (state, action)=>{
-          const newItem = action.payload;
-          const findIndex = state.bagItems.findIndex(x => x.id === newItem.id);
-          if(findIndex < 0){
-            state.bagItems.push(
-              {
-                id: newItem.id,
-                image: newItem.image,
-                name: newItem.name,
-                price: newItem.price,
-                amount: 1
-              })
-            }else{
-              state.bagItems[findIndex].price += action.payload.price
-              state.bagItems[findIndex].amount +=1; 
-            }
+  name: 'bag',
+  initialState,
+  reducers: {
+    addToBag: (state, action) => {
+      const newItem = action.payload;
+      const findIndex = state.bagItems.findIndex(x => x.id === newItem.id);
+      if (findIndex < 0) {
+        state.bagItems.push(
+          {
+            id: newItem.id,
+            image: newItem.image,
+            name: newItem.name,
+            price: newItem.price,
+            amount: 1
+          })
+      } else {
+        state.bagItems[findIndex].amount += 1;
+      }
+      state.totalAmount += newItem.price
+    },
+    increaseItemAmount: (state, action) => {
+      const newItem = action.payload;
+      const findIndex = state.bagItems.findIndex(x => x.id === newItem.id);
+      state.bagItems[findIndex].amount += 1;
+      state.totalAmount += newItem.price
+    },
 
-           state.totalAmount += newItem.price 
-        },
-        removeFromBag : (state,action)=>{
-          state.bagItems.push(action.payload) ;
-        },
-        showBag : (state, action)=>{
-          console.log(state.bagIsVisible)
-          state.bagIsVisible = action.payload
-        }
+    reduceItemAmount: (state, action) => {
+      const newItem = action.payload;
+      const findIndex = state.bagItems.findIndex(x => x.id === newItem.id);
+      state.bagItems[findIndex].price -= action.payload.price
+      if (state.bagItems[findIndex].amount === 1) {
+        delete state.bagItems[findIndex]
+      } else {
+        state.bagItems[findIndex].amount -= 1;
+      }
+      state.totalAmount -= newItem.price
     }
+    ,
+    removeFromBag: (state, action) => {
+      // state.bagItems.push(action.payload);
+    },
+    showBag: (state, action) => {
+      // state.bagIsVisible = action.payload
+    }
+  }
 })
 
 bagSlice.getInitialState()
